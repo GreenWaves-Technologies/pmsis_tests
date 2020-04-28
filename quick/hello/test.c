2bf3241c6a2453225cb840e4b3b0804a8b83e3cb
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2020-04-28 22:09:20 +0200

    Added variants of hello with uart and cluster

--- quick/hello/test.c
+++ quick/hello/test.c
@@ -10,9 +10,39 @@
 
 #include "pmsis.h"
 
+void pe_entry(void *arg)
+{
+    printf("Hello from (%d, %d)\n", pi_cluster_id(), pi_core_id());
+}
+
+void cluster_entry(void *arg)
+{
+    pi_cl_team_fork(0, pe_entry, 0);
+}
+
+
 static int test_entry()
 {
+#if defined(CLUSTER)
+    struct pi_device cluster_dev;
+    struct pi_cluster_conf cl_conf;
+    struct pi_cluster_task cl_task;
+
+    pi_cluster_conf_init(&cl_conf);
+    pi_open_from_conf(&cluster_dev, &cl_conf);
+    if (pi_cluster_open(&cluster_dev))
+    {
+        return -1;
+    }
+
+    pi_cluster_send_task_to_cl(&cluster_dev, pi_cluster_task(&cl_task, cluster_entry, NULL));
+
+    pi_cluster_close(&cluster_dev);
+#endif
+
     printf("Hello\n");
+
+    return 0;
 }
 
 static void test_kickoff(void *arg)
